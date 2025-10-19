19d98ccd7761b9bd20791668745173a09fd1c122
Fatima Amir <fatimaamir@Fatimas-MacBook-Air.local>
2025-10-19 22:34:56 +0900

    js open and close

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const editProfileBtn = document.querySelector(".profile__edit-btn");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+
+const newPostBtn = document.querySelector(".profile__add-btn");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+
+editProfileBtn.addEventListener("click", function () {
+  editProfileModal.classList.add("modal__is-opened");
+});
+editProfileCloseBtn.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal__is-opened");
+});
+
+newPostBtn.addEventListener("click", function () {
+  newPostModal.classList.add("modal__is-opened");
+});
+newPostCloseBtn.addEventListener("click", function () {
+  newPostModal.classList.remove("modal__is-opened");
+});
+
+/*const document.querySelector()
+edit-profile-modal
+new-post-modal
+modal__is-opened*/
